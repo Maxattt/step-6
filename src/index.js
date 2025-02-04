a55128108b03907bd4939088aa33726f4841d4e0
Maxattt <113494886+Maxattt@users.noreply.github.com>
2025-02-05 03:57:10 +0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppHeader from "./components/app-header";
+import SearchPanel from "./components/search-panel";
+import TodoList from "./components/todo-list";
+
+
+const App = () => {
+
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList />
+        </div>
+    );
+};
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
